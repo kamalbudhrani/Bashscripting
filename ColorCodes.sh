a2704ebcec10a23acc6ffcc3acd07255e0db2679
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-07 15:48:09 +0530

    My first Code

---
+++ ColorCodes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Color	Foreground	Background
+# Default	\033[39m	\033[49m
+# Black	\033[30m	\033[40m
+# Dark red	\033[31m	\033[41m
+# Dark green	\033[32m	\033[42m
+# Dark yellow (Orange-ish)	\033[33m	\033[43m
+# Dark blue	\033[34m	\033[44m
+# Dark magenta	\033[35m	\033[45m
+# Dark cyan	\033[36m	\033[46m
+# Light gray	\033[37m	\033[47m
+# Dark gray	\033[90m	\033[100m
+# Red	\033[91m	\033[101m
+# Green	\033[92m	\033[101m
+# Orange	\033[93m	\033[103m
+# Blue	\033[94m	\033[104m
+# Magenta	\033[95m	\033[105m
+# Cyan	\033[96m	\033[106m
+# White	\033[97m	\033[107m
+
+# Syntax : echo -e "\e[32m Your Text \e[0m"
+
+echo -e "\e[32m My name is Kamal \e[0m"

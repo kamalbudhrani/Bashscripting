f3c978a2163ab1891d567dc2b88741f033a43889
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-07 15:40:06 +0530

    My first Code

--- ShebangNotation.sh
+++ ShebangNotation.sh
@@ -3,3 +3,10 @@
 echo Hello World
 echo Line1
 echo Line2
+
+# Special Characters in Linux for specefic purpose
+#\n : Cursor moves to next line
+#\t : Cursor moves to tab space
+#Whenever you want to use special character, ensure you enable them with -e option in echo
+
+echo -e "Hello Kamal \nLine1 \nLine2"

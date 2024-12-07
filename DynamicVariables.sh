ab1193992df013ecf58f3396cc030dafbc49f12a
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-07 16:38:26 +0530

    My first Code

--- DynamicVariables.sh
+++ DynamicVariables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-Today_Date="$(date +%F)"
+# Expressions are always enclosed in Paranthesis
 
-echo "Hello, Good Morning Todays date is $Today_Date"
+Today_Date="$(date +%F)"
+No_of_Users="$(who | wc -l)"
+
+echo "Hello, Good Morning Todays date is ${Today_Date}"
+echo "Number of sessions opened are ${No_of_Users}"

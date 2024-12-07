8a95ee8e2323df3e725af6bba90ff8397037e534
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-07 16:03:16 +0530

    My first code

--- Variables.sh
+++ Variables.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Variable is something which holds the values dynamically.
+
+a=10
+b=20
+
+# No concept of data types in shell-scripting
+# Everything is considered as string by default
+
+echo $a
+echo ${a}
+echo "$a"
+echo "$(b)"
+
+echo '$(a)'
+
+# Whenever you use single quotes on variable, the power of special character will be taken down
+# Here $ is a special character to print variables.

07ae6b5c24e6f1520a76e610b8d3a524914829bb
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-07 17:28:36 +0530

    My first Code

--- OSspecialvariables.sh
+++ OSspecialvariables.sh
@@ -2,9 +2,13 @@
 
 # Special variables in linux are : $0 to $n , $* , $# , $@ , $$
 
-echo "Name of the executed script is : $0" # Prints the script name that you are running
-echo "First argument from command line : $1"
-echo "Second argument from command line : $2"
+echo $0 # Prints the script name that you are running
+echo $1 # Prints First argument from command line
+echo $2 # Prints Second argument from command line
+
+echo $*     # Gives you all the arguments used in the script
+echo $@     # Gives you all the arguments used in the script
+echo $#     # Gives you the number of arguments used
 
 # On command you can supply 9 variables
 # Example : sh abc.sh 10 20 30 40

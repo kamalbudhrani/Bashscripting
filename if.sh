2e315bc2b5adc7c3eac2fefa6770049028321941
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-08 20:12:50 +0530

    My first Code

--- if.sh
+++ if.sh
@@ -12,7 +12,7 @@ fi
 
 # Demo on not equals string operator
 
-if [ "$b" != "abcd"]; then
+if [ "$b" != "abcd" ]; then
     echo -e "\e[31m Both of them are not equal \e[0m"
 fi
 
@@ -24,11 +24,11 @@ fi
 
 c=$1
 
-if ["$c" -eq "10"]; then
+if [ "$c" -eq "10" ]; then
     echo "Value of c is 10"
-elif ["$c" -eq "20"]; then
+elif [ "$c" -eq "20" ]; then
     echo "Value of c is 20"
-elif ["$c" -eq "30"]; then
+elif [ "$c" -eq "30" ]; then
     echo "Value of c is 30"
 else
     echo "Value of c is not 10 or 20 or 30"

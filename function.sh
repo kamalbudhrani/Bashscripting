7ff0e266bd31635d8edafd5a755cc4daa4f2b90f
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-08 18:53:09 +0530

    My first Code

--- function.sh
+++ function.sh
@@ -7,5 +7,12 @@ sample() {
 }
 
 # Calling function sample
+sample
 
+stat() {
+    echo "Load average on the system is $(uptime | awk -F : '{print$NF}' | awk -F , '{print$1}')"
+    echo "Number of logged in sessions are $(who | wc -l)"
+    echo "..........Calling sample function..............."
+    sample   # Calling sample function inside stat function
+}
 

ee86f3f0f2212fbe3c6b5d1176e9bc0f2529bdb6
Kamal Budhrani <kamalbudhrani@gmail.com>
2024-12-14 17:05:46 +0530

    My first Code

--- Automation/Components/frontend.sh
+++ Automation/Components/frontend.sh
@@ -14,7 +14,7 @@ fi
 
 # This function is created to avoid repetative coding for same action, here same action is to print success or failure of every command
 stat() {
-    if [ $1 -eq 0 ]; then
+    if [ $1 -eq 0 ] ; then
         echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
